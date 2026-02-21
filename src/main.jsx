950b747b1dd3e0b7c0f237e97bc9a888757c2e43
Marshall Zhang <123522167+MarshallZzzz@users.noreply.github.com>
2026-02-21 10:28:42 -0700

    Added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/next"
 import * as Three from 'three'
 import "./index.css";
 import App from './App.jsx'
@@ -8,5 +9,6 @@ import { Meteors } from './components/ui/shadcn-io/meteors/Meteors.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
